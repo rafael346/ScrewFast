dd13547dd8b516d19c2732a2105a80a32657296d
Rafael Sousa <rafael@cashtech.dev>
2024-06-27 16:50:32 -0300

    fix: adjusment on velocity of transition

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -34,7 +34,7 @@ export default {
     },
     extend: {
       animation: {
-        'loop-scroll': 'loop-scroll 50s linear infinite',
+        'loop-scroll': 'loop-scroll 10s linear infinite',
       },
       keyframes: {
         'loop-scroll': {
